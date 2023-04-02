5315b7aff08dbdfc0b191a6b8a9fbb374a82f8e5
Burak Ocak <ocakbura@gmail.com>
2023-04-02 17:18:10 +0300

    feat: useEffect örnek1 yapıldı.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,38 @@
-
+import { useEffect, useState } from "react";
 
 function App() {
+  const [pencereBoyutu, pencereBoyutuGuncelle] = useState({
+    genislik: window.innerWidth,
+    yukseklik: window.innerHeight,
+  });
+
+  // useEffect( ()=> {
+    
+  //   function boyutlandirmaKodu() {
+  //     pencereBoyutuGuncelle({
+  //       genislik: window.innerWidth,
+  //       yukseklik: window.innerHeight,
+  //     });
+  //   }
+
+  //   window.addEventListener("resize", boyutlandirmaKodu);
+
+  //   //cleanup function
+  //   return ()=> {
+  //     window.removeEventListener("resize", boyutlandirmaKodu)
+  //   }
+
+  // } )
+
+  useEffect( ()=>{
+    pencereBoyutuGuncelle( {genislik: 100, yukseklik: 200} )
+  } )
+
   return (
-    <p>Merhaba</p>
+    <div>
+      <p>Pencere genişliği: {pencereBoyutu.genislik}</p>
+      <p>Pencere yüksekliği: {pencereBoyutu.yukseklik}</p>
+    </div>
   );
 }
 
