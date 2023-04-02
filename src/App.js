699c13c1d27e2dd1d36d75d856b24e384815a32f
Burak Ocak <ocakbura@gmail.com>
2023-04-02 20:55:13 +0300

    feat: useEfeect ve dep. array örneği yapıldı.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
+import { useEffect, useState } from "react";
 
 
 function App() {
+  const [sayac, sayacGuncelle] = useState(0);
+
+  const [ad, adGuncelle] = useState("Ad tanımsız.")
+
+  useEffect( ()=>{
+    document.title = sayac;
+  }, [sayac] );
+
+  function adFonksiyonu(){
+    let zaman = new Date().getSeconds();
+    adGuncelle("Ahmet Yılmaz: " + zaman);
+  }
+
   return (
-    <p>Merhaba</p>
+    <>
+      <p>{sayac}</p>
+      <button onClick={ ()=> {sayacGuncelle(onceki => onceki + 1)} }>Arttır</button>
+      <p>Ad: {ad}</p>
+      <button onClick={adFonksiyonu}>Ad Güncelle</button>
+    </>
   );
 }
 
